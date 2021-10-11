4fd90f4b5052d821eb8c8df40cead853a46a41cb
Diego San German <diegosange3@gmail.com>
2021-10-10 22:08:21 -0500

    Imagenes no utilizadas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import '@styles/Login.scss';
 import AuthenticationButton from '@components/authentication-button';
+import logo from '@logos/create-react-app.svg';
 
 const Login = () => {
 
 	return (
 		<div className="Login">
 			<div className="Login-container">
-				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+				<img src={logo} alt="logo" className="logo" />
 				<h1 className="title">Bienvenido a ¡Que postre!</h1>
 				<h2 className="title">Para ver el catalogo debes iniciar sesion</h2>
 
